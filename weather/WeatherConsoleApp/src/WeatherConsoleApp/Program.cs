a3fa5313aebe122f1e35b379dfb7cc83ac4fd9dc
Wes Higbee <wes.mcclure@gmail.com>
2016-10-05 15:43:35 -0400

    Exercise files for weather functions in console app - starting point

--- weather/WeatherConsoleApp/src/WeatherConsoleApp/Program.cs
+++ weather/WeatherConsoleApp/src/WeatherConsoleApp/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WeatherConsoleApp
+﻿namespace WeatherConsole
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-			Console.WriteLine("Hello World!");
+	using System.Threading.Tasks;
+	using static System.Console;
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Run().Wait();
 		}
-    }
+
+		private static async Task Run()
+		{
+			var city = "New York, NY";
+			var client = new OpenWeatherMapClient();
+			WriteLine($"Fetching weather for {city}");
+			var weather = await client.GetCurrentWeatherByCity(city);
+			if (weather == null)
+			{
+				WriteLine("Failed to fetch weather information.");
+				return;
+			}
+			WriteLine($"\nTemp: {weather.Main?.Temperature}");
+			WriteLine($"Low: {weather.Main?.MinTemperature}");
+			WriteLine($"High: {weather.Main?.MaxTemperature}");
+			WriteLine($"Humidity: {weather.Main?.Humidity}%");
+			WriteLine($"Condition: {weather.FirstCondition?.Description}");
+		}
+	}
 }
