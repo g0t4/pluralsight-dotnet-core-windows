c7f988ba43be22287d3c1e771c5ab690c21c584c
Wes Higbee <wes.mcclure@gmail.com>
2016-10-11 20:28:32 -0400

    Add in a pre-built MainPage with a very basic print out of weather information just to see xplat mobile working

--- xamarin/XamarinWeather/XamarinWeather/XamarinWeather/MainPage.xaml.cs
+++ xamarin/XamarinWeather/XamarinWeather/XamarinWeather/MainPage.xaml.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace XamarinWeather
+﻿namespace XamarinWeather
 {
+	using System.Threading.Tasks;
+	using OpenWeatherMapClient;
+	using Xamarin.Forms;
+
 	public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			await Run();
+		}
+
+		private async Task Run()
+		{
+			Weather.Text = "";
+
+			var city = "Lincoln, NE";
+			var client = new OpenWeatherMapClient();
+			var weather = await client.GetCurrentWeatherByCityAsync(city);
+
+			Weather.Text += $"Temp: {weather?.Main?.Temperature}\n";
+			Weather.Text += $"Low: {weather?.Main?.MinTemperature}\n";
+			Weather.Text += $"High: {weather?.Main?.MaxTemperature}\n";
+			Icon.Source = weather.WeatherIconUrl();
 		}
 	}
 }
