c6669aa4b6ced8a693faa2b6da7747029d1fd43d
Wes Higbee <wes.mcclure@gmail.com>
2016-10-12 00:37:58 -0400

    Write a test of building an IconUrl

--- weather/test/OpenWeatherMapClientTests/Tests.cs
+++ weather/test/OpenWeatherMapClientTests/Tests.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
+using OpenWeatherMapClient;
 
 namespace Tests
 {
-    public class Tests
-    {
-        [Fact]
-        public void Test1() 
-        {
-            Assert.True(true);
-        }
-    }
+	public class Tests
+	{
+		[Fact]
+		public void Test1()
+		{
+			var condition = new WeatherCondition();
+
+			condition.Icon = "2d";
+
+			Assert.Equal(condition.IconUrl, "http://openweathermap.org/img/w/2d.png");
+		}
+	}
 }
